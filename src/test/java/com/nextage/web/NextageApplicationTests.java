e459741e6927a7cda3f8c3ecca5eee4e538beea3
wefefwf <kidziro09@gmail.com>
2026-03-06 10:20:12 +0900

    first commit

---
+++ src/test/java/com/nextage/web/NextageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextage.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
