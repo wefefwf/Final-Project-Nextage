5cd25cb468068915d8aa8d574a6660e27a70302e
hyein <jinly8538@gmail.com>
2026-03-13 10:51:36 +0900

    고객페이지링크변경

--- src/main/java/com/nextage/web/controller/CustomerCartController.java
+++ src/main/java/com/nextage/web/controller/CustomerCartController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/cart")
+@RequestMapping("/customer/cart")
 @RequiredArgsConstructor
 public class CustomerCartController {
 
